36a30193d35a6b91d859655f5d6989c4c77a4714
noahho <Noah.homa@gmail.com>
2016-03-12 18:30:06 +0100

    234

--- app/src/main/java/com/example/noahh_000/starthack/CallLoadingActivity.java
+++ app/src/main/java/com/example/noahh_000/starthack/CallLoadingActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,6 +59,7 @@ public class CallLoadingActivity extends AppCompatActivity {
                             }
                             else
                             {
+
                                 ((TextView)findViewById(R.id.waitText)).setText(helperList.size()+" users were contacted.");
                             }
                         } else {
