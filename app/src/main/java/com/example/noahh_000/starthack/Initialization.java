f95f1c92dd9128c77fc06cbd1ac065ea8deecaa6
noahho <Noah.homa@gmail.com>
2016-03-12 08:55:41 +0100

    Few changes
    yooo first

---
+++ app/src/main/java/com/example/noahh_000/starthack/Initialization.java
@@ -0,0 +1,35 @@
+package com.example.noahh_000.starthack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+public class Initialization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initialization);
+
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+        ParseUser.enableAutomaticUser(); // Creates an anonymous user if not existent before, user is always logged in afterwards
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        if (currentUser.getString("type") == "") // User was not set before
+        {
+            Intent intent = new Intent(this, MainActivity.class); // Start type picking acticity
+        }
+        else if (currentUser.getString("type") == "translator")
+        {
+            Intent intent = new Intent(this, MainActivity.class); // Start translator activity
+        }
+        else if (currentUser.getString("type") == "user")
+        {
+            Intent intent = new Intent(this, MainActivity.class); // Start user activity
+        }
+    }
+}
---
+++ app/src/main/java/com/example/noahh_000/starthack/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.noahh_000.starthack;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseUser;
+
+import java.text.ParseException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+        ParseUser.enableAutomaticUser(); // Creates an anonymous user if not existent before, user is always logged in afterwards
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
