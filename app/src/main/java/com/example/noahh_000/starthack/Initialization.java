f525ffbbac37d2c86b9a0d319705a9fbc1cc0ff7
SamuelGabriel <muelsamu@student.ethz.ch>
2016-03-12 17:13:24 +0100

    Notification Sign Up, Cool Things

--- app/src/main/java/com/example/noahh_000/starthack/Initialization.java
+++ app/src/main/java/com/example/noahh_000/starthack/Initialization.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseUser;
+import com.onesignal.OneSignal;
+
 
 public class Initialization extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        OneSignal.startInit(this).init();
         setContentView(R.layout.activity_initialization);
 
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
@@ -22,8 +26,13 @@ public class Initialization extends AppCompatActivity {
         ParseUser.getCurrentUser().saveInBackground();
 
         ParseUser currentUser = ParseUser.getCurrentUser();
+        if(currentUser.getString("type") == null){
+            Intent intentApp = new Intent(this,
+                    MainActivity.class);
 
-        if (currentUser.getString("type") == "") // User was not set before
+            this.startActivity(intentApp);
+        }
+        else if (currentUser.getString("type").equals("")) // User was not set before
         {
             Intent intentApp = new Intent(this,
                     MainActivity.class);
@@ -31,23 +40,27 @@ public class Initialization extends AppCompatActivity {
             this.startActivity(intentApp);
 
         }
-        else if (currentUser.getString("type") == "translator")
+        else if (currentUser.getString("type").equals( "translator"))
         {
-            Intent intent = new Intent(this, HelperIntroActivity.class); // Start translator activity
-            this.startActivity(intent);
+            if (currentUser.getList("languages").isEmpty()){
+                Intent intent = new Intent(this, HelperIntroActivity.class); // Start translator activity
+                this.startActivity(intent);
+            }else{
+                Intent intent = new Intent(this, ReadyTranslatorActivity.class);
+                this.startActivity(intent);
+            }
+
 
         }
 
-        else if (currentUser.getString("type") == "user")
+        else if (currentUser.getString("type").equals("user"))
         {
             Intent intent = new Intent(this, HelpedPickerActivity.class); // Start user activity
             this.startActivity(intent);
 
         }else{
-            Intent intentApp = new Intent(this,
-                    MainActivity.class);
-
-            this.startActivity(intentApp);
+            Toast t =  Toast.makeText(getApplicationContext(),"Error",5);
+            t.show();
         }
     }
 }
